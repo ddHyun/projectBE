ccb254b733a54a679af21377f77f08716caed27c
ddHyun <ddHyun@github.com>
2023-11-21 17:37:36 +0900

    프로젝트 설정

---
+++ project/src/main/java/org/koreait/configs/SecurityConfig.java
@@ -0,0 +1,23 @@
+package org.koreait.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //활성화 무력화
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ project/src/main/java/org/koreait/entities/Base.java
@@ -0,0 +1,27 @@
+package org.koreait.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/*
+    날짜와 시간 등과 관련된 공통속성화
+ */
+@MappedSuperclass @Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Base {
+    @CreatedDate
+    @Column(updatable=false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime modifiedAt;
+}
