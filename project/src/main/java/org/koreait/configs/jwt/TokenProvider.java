798a0969c5d6bc8afb9649ab2f813f82853da1f5
ddHyun <ddHyun@github.com>
2023-11-23 00:11:27 +0900

    jwt TokenProvider 토큰 발급/발급받은 토큰으로 회원정보 조회

--- project/src/main/java/org/koreait/configs/jwt/TokenProvider.java
+++ project/src/main/java/org/koreait/configs/jwt/TokenProvider.java
@@ -1,16 +1,32 @@
 package org.koreait.configs.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.koreait.models.member.MemberInfo;
+import org.koreait.models.member.MemberInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TokenProvider {
     //한 번 설정하면 바뀌지 않도록 상수(final)로 설정 -> 내부에서 못 바꾸게 막기
     private final String secret;
     private final Long accessTokenValidityInSeconds;
     private Key key;
+
+    @Autowired
+    private MemberInfoService infoService;
 
     public TokenProvider(String secret, Long accessTokenValidityInSeconds){ //HMAC(해시) : SHA512 + Message(추가검증)
         this.secret = secret; //인코딩되어 있는 상태라 디코딩 후 hmac
@@ -22,6 +38,38 @@ public class TokenProvider {
 
     /*회원정보를 담은 토큰 만들기*/
     public String createToken(Authentication authentication){
-        return null;
+        String authories = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        //getTime():현재시간
+        Date expires = new Date((new Date()).getTime()+accessTokenValidityInSeconds * 1000);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())//아이디
+                .claim("auth", authories)//접근권한
+                .signWith(key, SignatureAlgorithm.HS512) //HMAC + SHA512
+                .setExpiration(expires)//토큰유효시간(밀리세컨즈)
+                .compact();
+    }
+
+    /* 토큰을 Spring Security에 통합, 회원정보 가져오기 */
+    public Authentication getAuthentication(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJwt(token)
+                .getPayload();
+
+        String email = claims.getSubject();
+        MemberInfo userDetails = (MemberInfo) infoService.loadUserByUsername(email);
+
+        String auth = claims.get("auth").toString();
+        List<? extends GrantedAuthority> authorities = Arrays.stream(auth.split(","))
+                .map(SimpleGrantedAuthority::new).toList();
+        userDetails.setAuthorities(authorities);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, token, authorities);
+
+        return authentication;
     }
 }
