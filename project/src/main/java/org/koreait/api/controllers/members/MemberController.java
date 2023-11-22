4713a4eada7f2402a013475ff637307cf988431c
ddHyun <ddHyun@github.com>
2023-11-22 22:58:55 +0900

    메시지문구 관리/test서버

--- project/src/main/java/org/koreait/api/controllers/members/MemberController.java
+++ project/src/main/java/org/koreait/api/controllers/members/MemberController.java
@@ -1,6 +1,13 @@
 package org.koreait.api.controllers.members;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.koreait.commons.Utils;
+import org.koreait.commons.exceptions.BadRequestException;
+import org.koreait.commons.rests.JSONData;
+import org.koreait.models.member.MemberSaveService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,10 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/member")
+@RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberSaveService saveService;
+
     @PostMapping
-    public void join(@RequestBody @Valid RequestJoin form, Errors errors){
+    public ResponseEntity<JSONData> join(@RequestBody @Valid RequestJoin form, Errors errors){
         //@RequestBody : json형태로 데이터 내보내기
+        saveService.save(form, errors);
+
+        if(errors.hasErrors()){
+            throw new BadRequestException(Utils.getMessages(errors));
+        }
+
+        JSONData data = new JSONData();
+        data.setStatus(HttpStatus.CREATED);
+
+        return ResponseEntity.status(data.getStatus()).body(data);
     }
 }
--- project/src/main/java/org/koreait/commons/Utils.java
+++ project/src/main/java/org/koreait/commons/Utils.java
@@ -28,20 +28,26 @@ public class Utils {
         }
     }
 
-    public Map<String, List<String>> getMessages(Errors errors, String bundleType){
-        Map<String, List<String>> data = new HashMap<>();
-        for(FieldError error : errors.getFieldErrors()){   //key:field, value: 에러코드
-            String field = error.getField();
-            //에러코드 발생 시 NotBlank, NotBlank.email, NotBlank.requestJoin.email 등이 있다면 가장
-            //정확한 에러코드가 가장 긴 코드일테니 긴 순서대로 정렬. 실제 메시지코드에서 조회, 코드가 null이 아니면
-            //list형태로 반환
-            List<String> messages = Arrays.stream(error.getCodes()).sorted(Comparator.reverseOrder())
-                    .map(c -> getMessage(c, "validation"))
-                    .filter(c -> c != null)
-                    .toList();
+    public static Map<String, List<String>> getMessages(Errors errors){
 
-            data.put(field, messages);
+        try {
+            Map<String, List<String>> data = new HashMap<>();
+            for (FieldError error : errors.getFieldErrors()) {   //key:field, value: 에러코드
+                String field = error.getField();
+                //에러코드 발생 시 NotBlank, NotBlank.email, NotBlank.requestJoin.email 등이 있다면 가장
+                //정확한 에러코드가 가장 긴 코드일테니 긴 순서대로 정렬. 실제 메시지코드에서 조회, 코드가 null이 아니면
+                //list형태로 반환
+                List<String> messages = Arrays.stream(error.getCodes()).sorted(Comparator.reverseOrder())
+                        .map(c -> getMessage(c, "validation"))
+                        .filter(c -> c != null)
+                        .toList();
+
+                data.put(field, messages);
+            }
+            return data;
+        } catch(Exception e){
+            return null;
         }
-        return data;
+
     }
 }
