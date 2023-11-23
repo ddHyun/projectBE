e62a6b0412cede8d1a9c8c31b6bd0a69b4bcc3b3
ddHyun <ddHyun@github.com>
2023-11-23 19:13:08 +0900

    토큰

--- project/src/main/java/org/koreait/api/controllers/members/MemberController.java
+++ project/src/main/java/org/koreait/api/controllers/members/MemberController.java
@@ -5,14 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.commons.Utils;
 import org.koreait.commons.exceptions.BadRequestException;
 import org.koreait.commons.rests.JSONData;
+import org.koreait.models.member.MemberLoginService;
 import org.koreait.models.member.MemberSaveService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/member")
@@ -20,19 +19,42 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberSaveService saveService;
+    private final MemberLoginService loginService;
 
     @PostMapping
     public ResponseEntity<JSONData> join(@RequestBody @Valid RequestJoin form, Errors errors){
         //@RequestBody : json형태로 데이터 내보내기
         saveService.save(form, errors);
 
-        if(errors.hasErrors()){
-            throw new BadRequestException(Utils.getMessages(errors));
-        }
+        errorProcess(errors);
 
         JSONData data = new JSONData();
         data.setStatus(HttpStatus.CREATED);
 
         return ResponseEntity.status(data.getStatus()).body(data);
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity<JSONData> token(@RequestBody @Valid RequestLogin form, Errors errors){
+        errorProcess(errors);
+
+        String accessToken = loginService.login(form);
+
+        /* 토큰을 두 군데에 다 담았음
+        * 1. 응답바디 - JSONData 형식으로
+        * 2. 응답헤더 - Authorization : Bearer 토큰
+        */
+
+        JSONData data = new JSONData(accessToken);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer"+accessToken);
+
+        return ResponseEntity.status(data.getStatus()).headers(headers).body(data);
+    }
+
+    private void errorProcess(Errors errors){
+        if(errors.hasErrors()) {
+            throw new BadRequestException(Utils.getMessages(errors));
+        }
     }
 }
--- project/src/main/java/org/koreait/configs/SecurityConfig.java
+++ project/src/main/java/org/koreait/configs/SecurityConfig.java
@@ -28,25 +28,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(c -> c.disable())     //csrf토튼 해제, 이걸 사용하면 jwt토큰 사용 불가
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(customJwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class) //데이터공유를 다른 도메인에서도 가능하게
+                .addFilterBefore(customJwtFilter, UsernamePasswordAuthenticationFilter.class) //토큰이 요청헤더쪽에서 넘어오면 로그인 되게
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //세션 사용 안함
 
-        http.exceptionHandling(c -> {
-            c.authenticationEntryPoint((req, res, e) -> {
+        http.exceptionHandling(c -> {   //인가 실패시 유입되는 곳. 페이지 이동이 아닌 응답코드만 적기로 함
+            c.authenticationEntryPoint((req, res, e) -> {   //토큰이 없을 경우, 로그인 안했을 경우
                 res.sendError(HttpServletResponse.SC_UNAUTHORIZED); //401
             });
             
-            c.accessDeniedHandler((req, res, e) -> {
+            c.accessDeniedHandler((req, res, e) -> { //로그인은 했지만 특정 권한이 없는 경우
                 res.sendError(HttpServletResponse.SC_FORBIDDEN); //403
             });
         });
         
-        http.authorizeHttpRequests(c -> {
+        http.authorizeHttpRequests(c -> { //인가에 대한 주소관련 설정
            c.requestMatchers(
                    "/api/v1/member", //회원가입
-                   "/api/v1/member/token", //로그인
-                   "/api/v1/member/exists/**").permitAll()
+                   "/api/v1/member/token" //로그인
+//                   "/api/v1/member/exists/**"
+                   ).permitAll()  //인증 필요없이 다 사용가능
                    .anyRequest().authenticated(); //나머지 url은 모두 토큰인증(회원인증)해야함
         });
 
